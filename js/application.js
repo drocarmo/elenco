56e32f41304792f06b833c0ad3ce1a2d7fe2e265
drocarmo <pe@drocarmo.com>
2014-01-07 23:36:45 -0600

    responsive design is responsible design

--- js/application.js
+++ js/application.js
@@ -8,9 +8,9 @@ var storage = window.elencoStore;
 
 storage.getItems(function(data){
   if (data.length) {
-		$('li[data-default=yes]').remove();
-		fillList(data);
-	};
+    $('li[data-default=yes]').remove();
+    fillList(data);
+  };
 });
 
 $html = $("html");
@@ -24,7 +24,7 @@ $(".new input").bind('blur keyup',function(e) {
     if(item !== ""){
       var listitem = '<li>'
         + '<input value="' + item + '" placeholder="Add item..." />'
-        + '<a class="check-toggle">'
+        + '<a class="task">'
         + '<span class="close">&times;</span>'
         + '</a>'
         + '</li>'
@@ -32,26 +32,26 @@ $(".new input").bind('blur keyup',function(e) {
       $(this).val('').parent().after(listitem);
       $(this).focus();
       storage.addItem(item, function(e){
-				
-			});
+
+      });
     }
   }
 });
 
-$(document).on('click', '.check-toggle', function(){
+$(document).on('click', '.task', function(){
   $(this).parent().addClass("removed");
-	var item = $(this).siblings('input').val();
+  var item = $(this).siblings('input').val();
   storage.removeItem(item);
 });
 
 function fillList (data) {
-	data.forEach(function(item){
-	  var listitem = '<li>'
-		  + '<input value="' + item + '" placeholder="Add item..." />'
-		  + '<a class="check-toggle">'
-		  + '<span class="close">&times;</span>'
-		  + '</a>'
-		  + '</li>';
-	  $list.append(listitem);
-	});
+  data.forEach(function(item){
+    var listitem = '<li>'
+      + '<input value="' + item + '" placeholder="Add item..." />'
+      + '<a class="task">'
+      + '<span class="close">&times;</span>'
+      + '</a>'
+      + '</li>';
+    $list.append(listitem);
+  });
 };

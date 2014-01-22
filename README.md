705ec0f2d09f79168512ed768af6a8f532c2dd49
drocarmo <pe@drocarmo.com>
2014-01-22 00:19:36 -0600

    work started on an elenco landing page

--- README.md
+++ README.md
@@ -1,34 +1,4 @@
-## Elenco
+Elenco
+=========
 
-##### What Elenco is:
-- A Chrome-extension (Download it [here](https://chrome.google.com/webstore/detail/elenco/phbjhmbdcecahmckhhbdabmnhmjcpjab)).
-- Lists, in Italian.
-- A task helper, using standard to-do lists.
-- Syncs all Chrome browsers your google account is logged into.
-- All open-sourced and open to feedback/feature requests.
-- Built with Jekyll, Javascript and Sass.
-
-##### What Elenco isn’t:
-- A new way to remind yourself to brush your teeth. Maybe.
-- Nothing really, possibilities are endless.
-
-##### How To Contribute (Make sure you have Jekyll, Ruby and Sass installed before running it on your local machine):
-1. Fork it ( http://github.com/drocarmo/elenco/fork )
-2. Create your feature branch (`git checkout -b my-new-feature`)
-3. Commit your changes (`git commit -a -m 'Add some feature'`)
-4. Push to the branch (`git push origin my-new-feature`)
-5. Create new Pull Request
-
-##### Features (Add or remove tasks with real-time data syncing)
-
-![Add Task](http://i.imgur.com/cUVc8zN.gif)
-
-![Remove Task](http://i.imgur.com/LN8OpVr.gif)
-
-##### License
-
-Elenco is built by Pedro Carmo ([@drocarmo](https://twitter.com/drocarmo)) and TJ Krusinski ([@TJkrusinski](https://twitter.com/TJkrusinski)). We're open to any feedback or feature requests.
-
->>>>>>>
-
-MIT
+A Chrome-extension that helps you stay on track with what you are currently working on, using lists, or in italian, Elenco.
--- js/application.js
+++ js/application.js
@@ -1,65 +1,298 @@
 // *************
 //
+// Typer JS
+//
+// *************
+
+String.prototype.rightChars = function(n){
+  if (n <= 0) {
+    return "";
+  }
+  else if (n > this.length) {
+    return this;
+  }
+  else {
+    return this.substring(this.length, this.length - n);
+  }
+};
+
+(function($) {
+  var
+    options = {
+      highlightSpeed    : 20,
+      typeSpeed         : 100,
+      clearDelay        : 500,
+      typeDelay         : 200,
+      clearOnHighlight  : true,
+      typerDataAttr     : 'data-typer-targets',
+      typerInterval     : 2000
+    },
+    highlight,
+    clearText,
+    backspace,
+    type,
+    spanWithColor,
+    clearDelay,
+    typeDelay,
+    clearData,
+    isNumber,
+    typeWithAttribute,
+    getHighlightInterval,
+    getTypeInterval,
+    typerInterval;
+
+  spanWithColor = function(color, backgroundColor) {
+    if (color === 'rgba(0, 0, 0, 0)') {
+      color = 'rgb(255, 255, 255)';
+    }
+
+    return $('<span></span>')
+      .css('color', color)
+      .css('background-color', backgroundColor);
+  };
+
+  isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  };
+
+  clearData = function ($e) {
+    $e.removeData([
+      'typePosition',
+      'highlightPosition',
+      'leftStop',
+      'rightStop',
+      'primaryColor',
+      'backgroundColor',
+      'text',
+      'typing'
+    ]);
+  };
+
+  type = function ($e) {
+    var
+      // position = $e.data('typePosition'),
+      text = $e.data('text'),
+      oldLeft = $e.data('oldLeft'),
+      oldRight = $e.data('oldRight');
+
+    // if (!isNumber(position)) {
+      // position = $e.data('leftStop');
+    // }
+
+    if (!text || text.length === 0) {
+      clearData($e);
+      return;
+    }
+
+
+    $e.text(
+      oldLeft +
+      text.charAt(0) +
+      oldRight
+    ).data({
+      oldLeft: oldLeft + text.charAt(0),
+      text: text.substring(1)
+    });
+
+    // $e.text($e.text() + text.substring(position, position + 1));
+
+    // $e.data('typePosition', position + 1);
+
+    setTimeout(function () {
+      type($e);
+    }, getTypeInterval());
+  };
+
+  clearText = function ($e) {
+    $e.find('span').remove();
+
+    setTimeout(function () {
+      type($e);
+    }, typeDelay());
+  };
+
+  highlight = function ($e) {
+    var
+      position = $e.data('highlightPosition'),
+      leftText,
+      highlightedText,
+      rightText;
+
+    if (!isNumber(position)) {
+      position = $e.data('rightStop') + 1;
+    }
+
+    if (position <= $e.data('leftStop')) {
+      setTimeout(function () {
+        clearText($e);
+      }, clearDelay());
+      return;
+    }
+
+    leftText = $e.text().substring(0, position - 1);
+    highlightedText = $e.text().substring(position - 1, $e.data('rightStop') + 1);
+    rightText = $e.text().substring($e.data('rightStop') + 1);
+
+    $e.html(leftText)
+      .append(
+        spanWithColor(
+            $e.data('backgroundColor'),
+            $e.data('primaryColor')
+          )
+          .append(highlightedText)
+      )
+      .append(rightText);
+
+    $e.data('highlightPosition', position - 1);
+
+    setTimeout(function () {
+      return highlight($e);
+    }, getHighlightInterval());
+  };
+
+  typeWithAttribute = function ($e) {
+    var targets;
+
+    if ($e.data('typing')) {
+      return;
+    }
+
+    try {
+      targets = JSON.parse($e.attr($.typer.options.typerDataAttr)).targets;
+    } catch (e) {}
+
+    if (typeof targets === "undefined") {
+      targets = $.map($e.attr($.typer.options.typerDataAttr).split(','), function (e) {
+        return $.trim(e);
+      });
+    }
+
+    $e.typeTo(targets[Math.floor(Math.random()*targets.length)]);
+  };
+
+  // Expose our options to the world.
+  $.typer = (function () {
+    return { options: options };
+  })();
+
+  $.extend($.typer, {
+    options: options
+  });
+
+  //-- Methods to attach to jQuery sets
+
+  $.fn.typer = function() {
+    var $elements = $(this);
+
+    return $elements.each(function () {
+      var $e = $(this);
+
+      if (typeof $e.attr($.typer.options.typerDataAttr) === "undefined") {
+        return;
+      }
+
+      typeWithAttribute($e);
+      setInterval(function () {
+        typeWithAttribute($e);
+      }, typerInterval());
+    });
+  };
+
+  $.fn.typeTo = function (newString) {
+    var
+      $e = $(this),
+      currentText = $e.text(),
+      i = 0,
+      j = 0;
+
+    if (currentText === newString) {
+      console.log("Our strings our equal, nothing to type");
+      return $e;
+    }
+
+    if (currentText !== $e.html()) {
+      console.error("Typer does not work on elements with child elements.");
+      return $e;
+    }
+
+    $e.data('typing', true);
+
+    while (currentText.charAt(i) === newString.charAt(i)) {
+      i++;
+    }
+
+    while (currentText.rightChars(j) === newString.rightChars(j)) {
+      j++;
+    }
+
+    newString = newString.substring(i, newString.length - j + 1);
+
+    $e.data({
+      oldLeft: currentText.substring(0, i),
+      oldRight: currentText.rightChars(j - 1),
+      leftStop: i,
+      rightStop: currentText.length - j,
+      primaryColor: $e.css('color'),
+      backgroundColor: $e.css('background-color'),
+      text: newString
+    });
+
+    highlight($e);
+
+    return $e;
+  };
+
+  //-- Helper methods. These can one day be customized further to include things like ranges of delays.
+
+  getHighlightInterval = function () {
+    return $.typer.options.highlightSpeed;
+  };
+
+  getTypeInterval = function () {
+    return $.typer.options.typeSpeed;
+  },
+
+  clearDelay = function () {
+    return $.typer.options.clearDelay;
+  },
+
+  typeDelay = function () {
+    return $.typer.options.typeDelay;
+  };
+
+  typerInterval = function () {
+    return $.typer.options.typerInterval;
+  };
+})(jQuery);
+
+// *************
+//
 // Item input JS
 //
 // *************
 
-var storage = window.elencoStore;
-var $list = $("ul");
+$html = $("html");
+$list = $("ul");
 
-storage.getItems(function(data) {
-  if (data.length) {
-    $('li[data-default=yes]').remove();
-    fillList(data);
-  }
-});
+$(".new input").bind('blur keyup',function(e) {
+  if (e.type == 'blur' || e.keyCode == '13') {
 
-function fillList(data) {
-  data.forEach(function(item, i) {
-    var listitem = '<li>' +
-      '<input class="item" data-index="'+i+'" value="' + item + '" placeholder="Add item..." />' +
-      '<a class="close-toggle">' +
-      '<span class="close">&times;</span>' +
-      '</a>' +
-      '</li>';
-    $list.append(listitem);
-  });
-}
-
-$("#new").on('keyup',function(e) {
-  if (e.keyCode === 13) {
     var item = $(this).val();
 
-    if (item) {
-      var listitem = '<li>' +
-        '<input value="' + item + '" placeholder="Add item..." />' +
-        '<a class="close-toggle">' +
-        '<span class="close">&times;</span>' +
-        '</a>' +
-        '</li>';
+    if(item !== ""){
+      var listitem = '<li>'
+        + '<input value="' + item + '" placeholder="Add item..." />'
+        + '<a class="check-toggle">'
+        + '<span class="close">&times;</span>'
+        + '</a>'
+        + '</li>'
 
-      $(this).val('');
-      $list.append(listitem);
+      $(this).val('').parent().after(listitem);
       $(this).focus();
-
-      var index = 0;
-      $('input.item').each(function(el){
-        $(el).attr('data-index', index);
-        index += 1;
-      });
-      storage.addItem(item, $.noop);
     }
   }
 });
 
-$(document).on('keyup', 'input.item', function(){
-  var index = $(this).attr('data-index'),
-      text = $(this).val();
-  storage.updateItem(index, text, $.noop);
-});
-
-$(document).on('click', '.close-toggle', function() {
+$(document).on('click', '.check-toggle', function(){
   $(this).parent().addClass("removed");
-  var item = $(this).siblings('input').val();
-  storage.removeItem(item);
 });

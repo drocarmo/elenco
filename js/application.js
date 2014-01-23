aeb418d06fe8e389289b710e50c3aacfc69eba81
drocarmo <pe@drocarmo.com>
2014-01-23 01:13:09 -0600

    landing page commit, need sleep

--- js/application.js
+++ js/application.js
@@ -264,35 +264,3 @@ String.prototype.rightChars = function(n){
     return $.typer.options.typerInterval;
   };
 })(jQuery);
-
-// *************
-//
-// Item input JS
-//
-// *************
-
-$html = $("html");
-$list = $("ul");
-
-$(".new input").bind('blur keyup',function(e) {
-  if (e.type == 'blur' || e.keyCode == '13') {
-
-    var item = $(this).val();
-
-    if(item !== ""){
-      var listitem = '<li>'
-        + '<input value="' + item + '" placeholder="Add item..." />'
-        + '<a class="check-toggle">'
-        + '<span class="close">&times;</span>'
-        + '</a>'
-        + '</li>'
-
-      $(this).val('').parent().after(listitem);
-      $(this).focus();
-    }
-  }
-});
-
-$(document).on('click', '.check-toggle', function(){
-  $(this).parent().addClass("removed");
-});
